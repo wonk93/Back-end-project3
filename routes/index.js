5eb58b07a1af169125deb884cd4de03d4694bae6
wonk93 <danielsalasrodriguez93@gmail.com>
2023-07-19 21:04:39 +0200

    rutas back

--- routes/index.js
+++ routes/index.js
@@ -7,4 +7,19 @@ router.get("/", (req, res, next) => {
 });
 router.use("/auth", require("./auth.routes"));
 
+const profileRoutes = require("./profile.routes");
+router.use("/profile", profileRoutes);
+
+const commentRoutes = require("./comment.routes");
+router.use("/comment", commentRoutes);
+
+const cloudinaryRoutes = require("./cloudinary.routes");
+router.use("/cloudinary", cloudinaryRoutes);
+
+const recipeRoutes = require("./recipe.routes");
+router.use("/recipe", recipeRoutes);
+
+const recipeRoutes = require("./upload.routes");
+router.use("/upload", recipeRoutes);
+
 module.exports = router;
